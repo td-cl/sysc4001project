0533321732b2cdb9a1331c89bb7aa94d3d3ddc30
Trac-Duy Chu-Le <tracduy750@gmail.com>
2025-09-24 13:17:05 -0400

    pull template files

---
+++ interrupts.cpp
@@ -0,0 +1,41 @@
+/**
+ *
+ * @file interrupts.cpp
+ * @author Trac-Duy Chu-Le 101220669, Zachary Dredge 101197514
+ *
+ */
+
+#include <interrupts.hpp>
+
+int main(int argc, char **argv)
+{
+
+  // vectors is a C++ std::vector of strings that contain the address of the ISR
+  // delays  is a C++ std::vector of ints that contain the delays of each device
+  // the index of these elements is the device number, starting from 0
+  auto [vectors, delays] = parse_args(argc, argv);
+  std::ifstream input_file(argv[1]);
+
+  std::string trace;     //!< string to store single line of trace file
+  std::string execution; //!< string to accumulate the execution output
+
+  /******************ADD YOUR VARIABLES HERE*************************/
+
+  /******************************************************************/
+
+  // parse each line of the input trace file
+  while (std::getline(input_file, trace))
+  {
+    auto [activity, duration_intr] = parse_trace(trace);
+
+    /******************ADD YOUR SIMULATION CODE HERE*************************/
+
+    /************************************************************************/
+  }
+
+  input_file.close();
+
+  write_output(execution);
+
+  return 0;
+}
